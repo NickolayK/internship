42a434e5852d38f640b478ee8b5dac8112b281a8
nick <n7412440@gmail.com>
2019-07-19 12:30:54 +0400

    dd missed semicolon )

--- script.js
+++ script.js
@@ -1,237 +1,238 @@
 
-    let apartmens = [], 
-        bookMarks = [],
-        CallbackRegistry = {},
-        currentPage = 1,
-        currentSearch ;
+let apartmens = [],
+    bookMarks = [],
+    CallbackRegistry = {},
+    currentPage = 1,
+    currentSearch;
 
-      
+
 function scriptRequest(url, onSuccess, onError) {
 
-    let scriptOk = false; 
-  
+    let scriptOk = false;
+
     const callbackName = 'cb' + String(Math.random()).slice(-6);
 
     url += '&callback=CallbackRegistry.' + callbackName;
-  
-    CallbackRegistry[callbackName] = function(data) {
-      scriptOk = true; 
-      delete CallbackRegistry[callbackName]; 
-      onSuccess(data); 
+
+    CallbackRegistry[callbackName] = function (data) {
+        scriptOk = true;
+        delete CallbackRegistry[callbackName];
+        onSuccess(data);
     };
-  
+
     function checkCallback() {
-      if (scriptOk) return; 
-      delete CallbackRegistry[callbackName];
-      onError(url);
+        if (scriptOk) return;
+        delete CallbackRegistry[callbackName];
+        onError(url);
     }
-  
+
     var script = document.createElement('script');
-  
+
     script.onload = script.onerror = checkCallback;
     script.src = url;
-  
+
     document.body.appendChild(script);
-  }
-  
-  function onGetApartment(data) {
-    apartmens =  data.response.listings;
+}
+
+function onGetApartment(data) {
+    apartmens = data.response.listings;
     const container = document.getElementById('listContainer');
     renderer(container, apartmens);
-  }
+}
 
-  function onShowMoreApartment( data){
+function onShowMoreApartment(data) {
 
     apartmens.push(...data.response.listings);
     const container = document.getElementById('listContainer');
-    
-    renderer(container , apartmens);
-  }
-  
-  function fail(url) {
-    alert( 'Ошибка при запросе ' + url );
-  }
-  
-        function onShowMore(value){
-            currentPage++;
 
-            let api = 'http://api.nestoria.co.uk/api?action=search_listings',
-                place = '&place_name=' + currentSearch,
-                listing_type = '&listing_type=rent',
-                json_uk = '&encoding=json&pretty=1'
-                page = '&page=' + currentPage,  
-                api_input = api + place + listing_type + json_uk +  page
+    renderer(container, apartmens);
+}
 
-            scriptRequest(api_input, onShowMoreApartment, fail);          
+function fail(url) {
+    alert('Ошибка при запросе ' + url);
+}
 
-        }
+function onShowMore(value) {
+    currentPage++;
 
-        function getApartment(value){
-            currentPage = 1;
-            currentSearch = value;
-            
-            let api = 'http://api.nestoria.co.uk/api?action=search_listings',
-                place = '&place_name=' + value,
-                listing_type = '&listing_type=rent',
-                json_uk = '&encoding=json&pretty=1',
-                page = '&page=1',         
-                api_input = api + place + listing_type + json_uk +  page;
+    let api = 'http://api.nestoria.co.uk/api?action=search_listings',
+        place = '&place_name=' + currentSearch,
+        listing_type = '&listing_type=rent',
+        json_uk = '&encoding=json&pretty=1',
+        page = '&page=' + currentPage,
+        api_input = api + place + listing_type + json_uk + page;
+
+    scriptRequest(api_input, onShowMoreApartment, fail);
+
+}
+
+function getApartment(value) {
+    currentPage = 1;
+    currentSearch = value;
+
+    let api = 'http://api.nestoria.co.uk/api?action=search_listings',
+        place = '&place_name=' + value,
+        listing_type = '&listing_type=rent',
+        json_uk = '&encoding=json&pretty=1',
+        page = '&page=1',
+        api_input = api + place + listing_type + json_uk + page;
 
 
-            scriptRequest(api_input, onGetApartment, fail); 
-        }
+    scriptRequest(api_input, onGetApartment, fail);
+}
 
-function createElement( options ){
+function createElement(options) {
 
     const elem = document.createElement(options.elemName);
 
-    if(options.innerHTML){
+    if (options.innerHTML) {
         elem.innerHTML = options.innerHTML;
     }
 
-    if(options.eventName && options.handler ){
-        elem.addEventListener( options.eventName , options.handler);
+    if (options.eventName && options.handler) {
+        elem.addEventListener(options.eventName, options.handler);
     }
 
-    if(options.atribute  && options.atrValue){
+    if (options.atribute && options.atrValue) {
         elem.setAttribute(options.atribute, options.atrValue);
     }
-    
-    if( options.src){
-        elem.src =  options.src;
+
+    if (options.src) {
+        elem.src = options.src;
     }
 
     return elem;
 }
 
-        function openBookMark(){
+function openBookMark() {
 
-            let container = createElement({elemName : 'div'}),
-                closeBtn = createElement({elemName : 'button', innerHTML: 'Close' , eventName : 'click', handler : closeModal});
+    let container = createElement({ elemName: 'div' }),
+        closeBtn = createElement({ elemName: 'button', innerHTML: 'Close', eventName: 'click', handler: closeModal });
 
-            bookMarks.forEach( function(elem , index ){
-                let elemContainer =  createElement({elemName : 'div', atribute: 'id' , atrValue : index}),
-                    img = createElement({elemName : 'img', src: elem.img_url }),
-                    titleElem = createElement({elemName : 'h5', innerHTML: elem.title }),
-                    bottomContainer = createElement({elemName : 'div'}),
-                    price = createElement({elemName : 'span', innerHTML: 'Price : ' + elem.price_formatted }),
-                    deleteBtn = createElement({elemName : 'button', innerHTML: 'Delete From BookMarks' , eventName : 'click' , handler : deleteBookMark })
-                            
-                bottomContainer.appendChild(price);
-                bottomContainer.appendChild(deleteBtn);
+    bookMarks.forEach(function (elem, index) {
 
-                elemContainer.appendChild(titleElem);              
-                elemContainer.appendChild(img);
-                elemContainer.appendChild(bottomContainer);
+        let elemContainer = createElement({ elemName: 'div', atribute: 'id', atrValue: index }),
+            img = createElement({ elemName: 'img', src: elem.img_url }),
+            titleElem = createElement({ elemName: 'h5', innerHTML: elem.title }),
+            bottomContainer = createElement({ elemName: 'div' }),
+            price = createElement({ elemName: 'span', innerHTML: 'Price : ' + elem.price_formatted }),
+            deleteBtn = createElement({ elemName: 'button', innerHTML: 'Delete From BookMarks', eventName: 'click', handler: deleteBookMark })
 
-                container.appendChild(elemContainer);
+        bottomContainer.appendChild(price);
+        bottomContainer.appendChild(deleteBtn);
 
-            });
+        elemContainer.appendChild(titleElem);
+        elemContainer.appendChild(img);
+        elemContainer.appendChild(bottomContainer);
 
-            container.appendChild(closeBtn);
+        container.appendChild(elemContainer);
 
-            openModal(container);
-        }
+    });
 
-        function bookMarkToggle(event){
+    container.appendChild(closeBtn);
 
-            const index =  +event.target.parentElement.parentElement.getAttribute('id');
+    openModal(container);
+}
 
-            if(bookMarks.indexOf(apartmens[index]) === -1 ){
-                bookMarks.push(apartmens[index]);
-                event.target.innerText = 'delete from bookmark';
-            }else{
+function bookMarkToggle(event) {
 
-                let indexInBookMark = bookMarks.indexOf(apartmens[index]);
-                bookMarks.splice( indexInBookMark , 1 );
+    const index = +event.target.parentElement.parentElement.getAttribute('id');
 
-                event.target.innerText = ' to bookmark';
-            }
-            
-        }
+    if (bookMarks.indexOf(apartmens[index]) === -1) {
+        bookMarks.push(apartmens[index]);
+        event.target.innerText = 'delete from bookmark';
+    } else {
 
-        function deleteBookMark(event){
-            var index =  +event.target.parentElement.parentElement.getAttribute('id');
-            bookMarks.splice(index , 1 );
+        let indexInBookMark = bookMarks.indexOf(apartmens[index]);
+        bookMarks.splice(indexInBookMark, 1);
 
-        }
+        event.target.innerText = ' to bookmark';
+    }
 
+}
 
-        function renderer(container , displayedItems){
-     
-            container.innerHTML = '';
+function deleteBookMark(event) {
+    var index = +event.target.parentElement.parentElement.getAttribute('id');
+    bookMarks.splice(index, 1);
 
-            displayedItems.forEach( function(elem , index ){
-                let elemContainer =  createElement({elemName : 'div', atribute: 'id' , atrValue : index}),
-                    titleElem = createElement({elemName : 'h5', innerHTML: elem.title }),
-                    img = createElement({elemName : 'img', src: elem.img_url }),
-                    bottomContainer = createElement({elemName : 'div'}),
-                    price = createElement({elemName : 'span', innerHTML: 'Price : ' + elem.price_formatted }),
-                    detailBtn = createElement({elemName : 'button', innerHTML: 'Detail' , eventName : 'click' , handler : onDetail }),
-                    addBookMarkBtn = createElement({elemName : 'button', innerHTML: 'to bookmark' , eventName : 'click' , handler : bookMarkToggle });
+}
 
 
+function renderer(container, displayedItems) {
 
-                bottomContainer.appendChild(price);
-                bottomContainer.appendChild(detailBtn);
-                bottomContainer.appendChild(addBookMarkBtn);
-                
-                elemContainer.appendChild(titleElem);
-                elemContainer.appendChild(img);  
-                elemContainer.appendChild(bottomContainer);
+    container.innerHTML = '';
 
-                container.appendChild(elemContainer);
-            });
-        }
-
-
-        function onDetail(event){
-
-            const index =  +event.target.parentElement.parentElement.getAttribute('id');
-
-            let elem = apartmens[index],  
-                closeBtn =  createElement({elemName : 'button', innerHTML : 'Close', eventName:'click' , handler : closeModal}),
-                container = createElement({elemName : 'div'}),
-                titleElem = createElement({elemName : 'h5', innerHTML : 'Detail of : ' + elem.title });
+    displayedItems.forEach(function (elem, index) {
+        let elemContainer = createElement({ elemName: 'div', atribute: 'id', atrValue: index }),
+            titleElem = createElement({ elemName: 'h5', innerHTML: elem.title }),
+            img = createElement({ elemName: 'img', src: elem.img_url }),
+            bottomContainer = createElement({ elemName: 'div' }),
+            price = createElement({ elemName: 'span', innerHTML: 'Price : ' + elem.price_formatted }),
+            detailBtn = createElement({ elemName: 'button', innerHTML: 'Detail', eventName: 'click', handler: onDetail }),
+            addBookMarkBtn = createElement({ elemName: 'button', innerHTML: 'to bookmark', eventName: 'click', handler: bookMarkToggle });
 
 
-            container.appendChild(titleElem);
-            container.appendChild(closeBtn);   
 
-            openModal(container);
-            
-        }
+        bottomContainer.appendChild(price);
+        bottomContainer.appendChild(detailBtn);
+        bottomContainer.appendChild(addBookMarkBtn);
 
-        function openModal(container){
+        elemContainer.appendChild(titleElem);
+        elemContainer.appendChild(img);
+        elemContainer.appendChild(bottomContainer);
 
-            let modal = createElement({elemName : 'div' , atribute : 'id' , atrValue : 'modal'})
-            
-            modal.appendChild(container);
-            document.body.style.overflow = 'hidden'
-            document.body.appendChild(modal)
+        container.appendChild(elemContainer);
+    });
+}
 
-        }
 
-        function closeModal() {
-            document.body.style.overflow = 'auto'
-            document.body.removeChild(document.getElementById('modal'));
-          }
+function onDetail(event) {
 
-        function clearSearch(){
-            var input = document.getElementById('myInput');
-            input.value = '';
-            filteredDoList = null;
-        }
+    const index = +event.target.parentElement.parentElement.getAttribute('id');
 
-        function searchBtn(){
-            var value = document.getElementById('myInput').value;
-            if(value){
-                getApartment(value);
-            }          
-        }
+    let elem = apartmens[index],
+        closeBtn = createElement({ elemName: 'button', innerHTML: 'Close', eventName: 'click', handler: closeModal }),
+        container = createElement({ elemName: 'div' }),
+        titleElem = createElement({ elemName: 'h5', innerHTML: 'Detail of : ' + elem.title });
 
-        function find(event , value){
-            if(event.keyCode === 13 && value){
-                getApartment(value);
-            }
-        }
+
+    container.appendChild(titleElem);
+    container.appendChild(closeBtn);
+
+    openModal(container);
+
+}
+
+function openModal(container) {
+
+    let modal = createElement({ elemName: 'div', atribute: 'id', atrValue: 'modal' });
+
+    modal.appendChild(container);
+    document.body.style.overflow = 'hidden';
+    document.body.appendChild(modal);
+
+}
+
+function closeModal() {
+    document.body.style.overflow = 'auto'
+    document.body.removeChild(document.getElementById('modal'));
+}
+
+function clearSearch() {
+    var input = document.getElementById('myInput');
+    input.value = '';
+    filteredDoList = null;
+}
+
+function searchBtn() {
+    var value = document.getElementById('myInput').value;
+    if (value) {
+        getApartment(value);
+    }
+}
+
+function find(event, value) {
+    if (event.keyCode === 13 && value) {
+        getApartment(value);
+    }
+}
